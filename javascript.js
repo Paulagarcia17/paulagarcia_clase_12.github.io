1dfe36219452d849d3c2457147cd984301eaf937
Paulagarcia17 <130710710+Paulagarcia17@users.noreply.github.com>
2023-05-26 14:04:22 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,15 @@
+
+
+//let saludo = prompt ("Nombre de Usuario")
+
+
+
+function saludo (persona) {
+
+   alert ("Bienvenid@"+ " " +  persona)
+    
+}
+
+saludo ("Maria")  ;
+
+//No me anda el prompt y function juntos - Si comento uno me anda el otro 
